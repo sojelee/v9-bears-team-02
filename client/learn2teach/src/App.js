532b95907bd1c7c164da3d587c0c826880da830c
Joseph Myalla <chanangae@gmail.com>
2019-06-24 15:12:21 +0300

    minor updates

--- client/learn2teach/src/App.js
+++ client/learn2teach/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
               
           Learn 2 Teach
+          <p>Join the community to learn and grow together</p>
         
       </header>
     </div>
